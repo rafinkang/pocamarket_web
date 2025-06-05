0067bf3d02ccf4a1b1b05d3961907ff7301f695f
Taeuk Kang <rkdxodnr07@gmail.com>
2025-06-05 17:01:16 +0900

    Update authStore.js

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,35 +1,48 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware"
+import { logout } from "@/api/login";
 
 const useAuthStore = create(
     // 상태를 영속적으로 저장하기 위한 미들웨어 (첫번째 인자: 상태값과 액션을 정의하는 함수, persist 옵션)
     persist(
         (set) => ({
             // 상태값
-            isLoggedIn: false, // 로그인 여부
+            isLogin: false, // 로그인 여부
             user: null, // 사용자 정보
 
             // 액션
             // 로그인 후
             login: (userData) => set({
-                isLoggedIn: true,
+                isLogin: true,
                 user: {
                     nickname: userData.nickname,
+                    status: userData.status,
+                    grade: userData.grade,
                     lastLoginAt: userData.lastLoginAt
                 }
             }),
 
             // 로그아웃 후
-            logout: () => set({
-                isLoggedIn: false,
-                user: null
-            })
+            logout: async () => {
+                try {
+                    const response = await logout()
+                    console.log('response ::: ', response)
+                    if (response.ok) {
+                        set({
+                            isLogin: false,
+                            user: null
+                        })
+                    }
+                } catch (error) {
+                    console.error('Error in logout server action:', error);
+                }
+            }
         }),
         {
             name: 'auth-storage', // sessionStorage에 저장될 고유 키 이름
             storage: createJSONStorage(() => sessionStorage), // sessionStorage 사용
             // 특정 상태만 저장
-            // partialize: (state) => ({ isLoggedIn: state.isLoggedIn, user: state.user }),
+            // partialize: (state) => ({ isLogin: state.isLogin, user: state.user }),
         }
     )
 );
