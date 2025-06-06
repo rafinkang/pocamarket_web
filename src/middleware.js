c279b1f17e1ca29fe0ca3d83ec805e8fc415f3bb
wldus904 <jypark2781@gmail.com>
2025-06-06 18:32:05 +0900

    feat: app router middleware 추가

---
+++ src/middleware.js
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import { LOGIN } from './constants/path';
+
+/** @param {import('next/server').NextRequest} request */
+export function middleware(request) {
+  // 1. 로그인 상태 확인 (오직 쿠키에서만 토큰을 가져옴.)
+  const accessToken = request.cookies.get('accessToken')?.value;
+  console.log('미들웨어에서 확인한 accessToken:', accessToken);
+
+  // 2. 로그인이 필요한 페이지에 접근하는데, 토큰이 없는 경우
+  // isLogin 대신 accessToken의 존재 여부로 판단.
+  if (!accessToken) {
+    const loginUrl = new URL(LOGIN, request.url);
+    loginUrl.searchParams.set('redirect', request.nextUrl.pathname);
+    
+    console.log('로그인이 필요하여 로그인 페이지로 리디렉션합니다.');
+    return NextResponse.redirect(loginUrl);
+  }
+
+  // 3. 토큰이 있는 경우, 요청을 그대로 진행
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    '/mypage/:path*', // mypage와 그 하위 모든 경로에 적용
+  ],
+};
