8d3fb14f9b40ec6e320c51bab7914119d5c4d458
wldus904 <jypark2781@gmail.com>
2025-06-16 17:27:02 +0900

    refactor: 테스트 컴포넌트 삭제

--- src/app/login-test/page.js
+++
@@ -1,7 +0,0 @@
-export default function LoginTest() {
-    return (
-        <div>
-            로그인 후 접근 가능 페이지
-        </div>
-    )
-}
--- src/app/logout-test/page.js
+++
@@ -1,7 +0,0 @@
-export default function LogoutTest() {
-    return (
-        <div>
-            로그아웃 상태에서도 접근 가능 페이지
-        </div>
-    )
-}
