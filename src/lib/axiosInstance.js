53249ea242a331579da097ba3a6e0fbae0aa22f5
wldus904 <jypark2781@gmail.com>
2025-06-20 20:08:55 +0900

    feat: baseURL 수정

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_LOCAL_API_URL
-// const baseURL = '/api/proxy';
+const baseURL = '/api/proxy';
 
 const axiosInstance = axios.create({
-    baseURL: `${baseURL}/api`,
-    // baseURL: baseURL,
+    baseURL: baseURL,
     timeout: 10000, // 요청 타임아웃 (10초)
     headers: {
         'Content-Type': 'application/json',
