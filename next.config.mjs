6276db3aaa9aa6b14311572944faef22d8b81a3c
Taeuk Kang <rkdxodnr07@gmail.com>
2025-06-28 17:54:36 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
 // 1. 위에서 생성한 상수 파일 가져오기
 import { S3_IMAGE_CONFIG } from './src/constants/config.js';
 
+const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
@@ -11,6 +12,9 @@ const nextConfig = {
   //   includePaths: [path.join(process.cwd(), 'src', 'styles')], // @import 시 기본 경로로 추가
   //   prependData: `@import "variables.scss";`, // 모든 SCSS 파일에 자동으로 특정 파일 import (예: 변수 파일)
   // },
+  compiler: {
+    removeConsole: isProduction ? { except: ['error'] } : false,
+  },
   images: {
     remotePatterns: [
       {
