852aa8a6b17b3d1de708b5049bf63a31d6d7d9e1
Taeuk Kang <rkdxodnr07@gmail.com>
2025-07-09 19:38:43 +0900

    Update FilterCardBox.jsx

--- src/app/pokemon-card-trade/components/Search/FilterCardBox.jsx
+++ src/app/pokemon-card-trade/components/Search/FilterCardBox.jsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { RiArrowLeftRightFill } from "react-icons/ri";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function FilterCardBox({ children }) {
   return (
-    <Card className="mb-6 border-2 border-gray-200 shadow-lg py-0 overflow-hidden gap-0">
+    <Card className="border-2 border-gray-200 shadow-lg py-0 overflow-hidden gap-0">
       <CardContent className="p-6 md:p-8">
         <div className="flex flex-col lg:flex-row items-center justify-center gap-6 lg:gap-12">
           {children}
