448732b880bb86810f54e56a98f0b6fe850261c3
Taeuk Kang <rkdxodnr07@gmail.com>
2025-07-10 15:46:26 +0900

    Fix: 로그인 Authprovider 에러 수정

--- src/components/providers/AuthProvider.jsx
+++ src/components/providers/AuthProvider.jsx
@@ -8,14 +8,23 @@ import { useEffect } from 'react';
  */
 export default function AuthProvider({ children }) {
   const clearUser = useAuthStore((state) => state.clear);
+  const user = useAuthStore((state) => state.user);
 
   useEffect(() => {
     // 앱 로드 시, 서버에 내 인증 상태를 물어보는 함수
     const checkAuthStatus = async () => {
       try {
-        const res = await fetch('/next-api/auth/me'); // next.js 서버의 refreshToken 존재 여부를 확인하는 API
+        const res = await fetch('/next-api/auth/me', {
+          method: 'POST',
+          body: JSON.stringify({
+            user: user
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }); // next.js 서버의 refreshToken 존재 여부를 확인하는 API
         const data = await res.json();
-
+        // user정보가 없거나, 리프레쉬 토큰이 없을경우 로그아웃 처리
         if (!data.isLogin) {
           clearUser();
         }
@@ -23,7 +32,6 @@ export default function AuthProvider({ children }) {
         console.error("인증 상태 확인 실패:", error);
         // 에러 발생 시에도 로그아웃 상태로 처리
         clearUser();
-
       }
     };
 
